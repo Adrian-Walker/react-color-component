fb9d92ba15c8c39d44a91b54c01e0273b0703258
Adrian Walker <awalker3365@gmail.com>
2021-09-11 12:57:00 -0400

    9-11-21 1256p

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+        
+    }
+  }
+}
+
+export default App;
