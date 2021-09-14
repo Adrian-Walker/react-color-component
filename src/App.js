eda14ab8a26d108772cd1321d44c2ccd87838bd5
Adrian Walker <awalker3365@gmail.com>
2021-09-14 11:54:37 -0400

    9-14-21 1154a

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from "react"
 import { Component } from "react";
+import axios from "axios";
 
+//App component
 class App extends Component {
   constructor() {
     super()
     this.state = {
-        
+      color: ""
     }
   }
+
+  //Axios get request
+  componentDidMount() {
+    axios.get(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`)
+      .then(res => {
+        // console.log(res);
+        this.setState({ color: res.data.colors[0].hex });
+      })
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        { }
+      </div>
+    )
+  }
+
 }
 
 export default App;
